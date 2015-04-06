c3b9268dc06894fb9c17482a0077d65a41adb72a
tkon99 <tkon99@gmail.com>
2015-04-06 17:07:32 +0200

    Added v1

---
+++ encyclapidi.class.php
@@ -0,0 +1,68 @@
+<?php
+class encyclapidi{
+	private $api_base = "http://www.encyclo.nl/app/zoek2.php?woord=";
+	private $api_device = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)";
+
+	public function __construct(){
+		if($this->get('test') == false){
+			throw new Exception('Encyclo is down, or not accepting requests on: "'.$this->api_base.'", stopping script.');
+			exit();
+		}
+		echo json_encode($this->getWord('test'));
+	}
+
+	public function getWord($word = false, $strip = false){
+		if($word == false){
+			throw new Exception('function getWord() needs a word as parameter.');
+		}else{
+			$raw = $this->get($word);
+			$suggestions = array();
+			$results = array();
+			if(array_key_exists("resultaten", $raw)){
+				//word found
+				$results = $raw["resultaten"];
+			}else{
+				//word not found
+				if(array_key_exists("welindedatabase", $raw)){
+					if(trim($raw["welindedatabase"]) !== ""){
+						//suggestions
+						foreach(explode('|', trim($raw["welindedatabase"])) as $suggestion){
+							if(trim($suggestion) !== "" && !empty($suggestion)){
+								$suggestions[] = $suggestion;
+							}
+						}
+					}else{
+						//no suggestions
+					}
+				}else{
+					//no suggestions
+				}
+			}
+			$return = array();
+			$return["results"] = $results;
+			$return["suggestions"] = $suggestions;
+
+			return $return;
+		}
+	}
+
+	private function get($param = ''){
+		$getUrl = $this->api_base.$param;
+		$device = $this->api_device;
+
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $getUrl,
+			CURLOPT_USERAGENT => $device
+		));
+		$resp = curl_exec($curl);
+		$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		//echo($httpcode);
+		curl_close($curl);
+
+		if($httpcode>=200 && $httpcode<300) return json_decode($resp, 1);
+		else return false;
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include('encyclapidi.class.php');
+$encyclo = new encyclapidi();
+//demo comming soon
+?>
